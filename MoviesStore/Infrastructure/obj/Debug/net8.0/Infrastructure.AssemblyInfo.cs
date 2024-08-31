9a09a5ca0fb1b9cb2ce041702ae47127d135c65b
carlosdev <carlosdevelopp@gmail.com>
2024-08-31 12:45:43 -0600

    Infrastructure

--- MoviesStore/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ MoviesStore/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85acf1849a16828fe0fa3260d195bf8eccdfb79b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a475913336dc22825133b206f8212f867d2cc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

08a413f037298ca9bb2b4d8e86c9e252432d429b
carlosdev <carlosdevelopp@gmail.com>
2024-09-22 01:32:10 -0600

    PutPendiente

--- MoviesStore/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ MoviesStore/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+573ad02f98491d6707d764f67d6b031728e81135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f7642d2b379c2401454a168891e7f5bc4aeff19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
